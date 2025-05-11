9926b39110e791d749b580691c3666f5cb6140e1
manas <>
2025-05-11 23:44:48 +0530

    feat: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import Intro from "./pages/Intro";
 import Blog from "./pages/Blog";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,8 +12,8 @@ const App = () => {
         <Route index element={<Intro />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
-
       <Navbar />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { cn } from "../lib/utils";
+
+import { Github } from "./icons/Github";
+import { Linkedin } from "./icons/Linkedin";
+import { Mail } from "./icons/Mail";
+import { Medium } from "./icons/Medium";
+
+const Footer = () => {
+  return (
+    <>
+      <footer
+        className={cn(
+          "relative mt-auto mb-2 w-full border-t-1 border-[#232B34]",
+        )}
+      >
+        <div className="wrapper">
+          <div className="w-full flex items-center justify-between p-2">
+            <p className="footnote-regular text-text-neutral">
+              © {new Date().getFullYear()} Manas. All rights reserved.
+            </p>
+            <div className="inline-flex items-center justify-end gap-2 p-1 self-stretch">
+              <Github className="w-5 h-5 text-text-neutral" />
+              <Linkedin className="w-5 h-5 text-text-neutral" />
+              <Mail className="w-5 h-5 text-text-neutral" />
+              <Medium className="w-5 h-5 text-text-neutral" />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
