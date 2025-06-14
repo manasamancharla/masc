8ea99724f2b453b5fcea63dffd08c139832f2163
manas <>
2025-06-14 23:35:15 +0530

    fix: add latest theme

--- src/components/icons/TechStack.tsx
+++ src/components/icons/TechStack.tsx
@@ -14,7 +14,7 @@ export function TechStack({ className, ...props }: SVGProps<SVGSVGElement>) {
     >
       <path
         d="M5.28615 8.69231L1 11L5.28615 13.3077M5.28615 8.69231L11 11.7692L16.7138 8.69231M5.28615 8.69231L1 6.38462L11 1L21 6.38462L16.7138 8.69231M5.28615 13.3077L1 15.6154L11 21L21 15.6154L16.7138 13.3077M5.28615 13.3077L11 16.3846L16.7138 13.3077M16.7138 8.69231L21 11L16.7138 13.3077"
-        stroke="white"
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
--- src/pages/_components/Bento/BentoBadge.tsx
+++ src/pages/_components/Bento/BentoBadge.tsx
@@ -1,5 +1,6 @@
 import type { JSX, SVGProps } from "react";
 import { cn } from "../../../lib/utils";
+import { useIsDarkTheme } from "@/hooks/useTheme";
 
 type SVGComponent = (props: SVGProps<SVGSVGElement>) => JSX.Element;
 
@@ -10,6 +11,8 @@ type BentoBadgeProps = {
 };
 
 const BentoBadge = ({ icon: Icon, label, className = "" }: BentoBadgeProps) => {
+  const isDark = useIsDarkTheme();
+
   return (
     <div
       className={cn(
@@ -17,8 +20,13 @@ const BentoBadge = ({ icon: Icon, label, className = "" }: BentoBadgeProps) => {
         className,
       )}
     >
-      <div className="w-10 h-10 bg-[#000000] flex justify-center items-center rounded-lg">
-        <Icon className="w-5 h-5" />
+      <div
+        className={cn(
+          "w-10 h-10 flex justify-center items-center rounded-lg",
+          isDark ? "bg-black" : "bg-white border-b-2 border-r-2",
+        )}
+      >
+        <Icon className={cn("w-5 h-5", isDark ? "text-white" : "text-black")} />
       </div>
       <p className="body-bold text-text-neutral">{label}</p>
     </div>
